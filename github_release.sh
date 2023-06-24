325472eb63159c38bceaadc973b2749961c361d4
flame-0 <fl4m3@pm.me>
2023-06-24 20:09:08 +0800

    Change support for one device only

--- github_release.sh
+++ github_release.sh
@@ -1,29 +1,22 @@
 #!/bin/env bash
 
-if [ "$#" -ne 3 ]
+if [ "$#" -ne 1 ]
 then
-  echo "You must provide three ZIPs for a device."
-  exit 1
+    echo "You must provide a zip for a device."
+    exit 1
 fi
 
 parse_device_name() {
-    parts=(${1//-/ })
-    echo ${parts[2]}
+    parts="(${1//-/ })"
+    echo "${parts}"
 }
 
-device_1=$(parse_device_name $1)
-device_2=$(parse_device_name $2)
-device_3=$(parse_device_name $3)
+device=$(parse_device_name "$1")
 
-if [ -z "$device_1" ]; then
+if [ -z "$device" ]; then
     echo "Not a valid zip."
     exit 1
 fi
 
-if [ "$device_1" != "$device_2" ] || [ "$device_1" != "$device_3" ]; then
-  echo "The three ZIPs are not for the same device!"
-  exit 1
-fi
-
 date=$(date +'%Y%m%d')
-gh release create "$device_1-$date" $1 $2 $3 --title "$device_1-$date build" --notes "Please read the changelog at https://kenvyra.xyz/devices/$device_1/ instead."
+gh release create "$device-$date" "$1" --title "$device-$date build" --notes "Please read the changelog at https://download.lineageos.org/devices/$device/changes instead."
