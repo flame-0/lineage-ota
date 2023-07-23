943d375d6361cafff513d2a5095f6ebd4d29d3a0
flame-0 <fl4m3@pm.me>
2023-07-23 20:07:23 +0800

    Allow customizable notes from input

--- github_release.sh
+++ github_release.sh
@@ -1,26 +1,45 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
-if [ "$#" -lt 1 ]; then
+if [[ "$#" -lt 1 ]]; then
     echo "You must provide a zip for a device."
     exit 1
 fi
 
-parse_device_name() {
-    echo "$1" | cut -d'-' -f3
-}
+device=$(echo "$1" | cut -d'-' -f3)
 
-device=$(parse_device_name "$1")
-
-if [ -z "$device" ]; then
+if [[ -z "$device" ]]; then
     echo "Not a valid zip."
     exit 1
 fi
 
 zip_date=$(echo "$1" | cut -d'-' -f5)
 
-for file in "$@"; do
+files=()
+notes=""
+process_notes=false
+for arg in "$@"; do
+    if [[ "$arg" == "-n" ]] || [[ "$arg" == "--note" ]]; then
+        process_notes=true
+    elif [[ "$process_notes" = true ]]; then
+        notes+="$arg"$'\n'
+    else
+        files+=("$arg")
+    fi
+done
+
+for file in "${files[@]}"; do
     sha256sum "$file" | head -c 64 > "$file.sha256sum"
     gh_args+=("$file" "$file.sha256sum")
 done
 
-gh release create "${device}-${zip_date}" "${gh_args[@]}" --title "${device}-${zip_date} build" --notes "Please read the changelog at https://download.lineageos.org/devices/${device}/changes instead."
+note_formatted=$(echo "$notes" | sed 's/^/- /' | sed '$ d')
+note_features="- Visit the [website](https://kenvyra.xyz/features/) for a complete list of features"
+note_default="Read the device changelog at https://download.lineageos.org/devices/${device}/changes."
+
+if [[ -n "$notes" ]]; then
+    notes="**Changelog**"$'\n\n'"${note_formatted}"$'\n'"${note_features}"$'\n\n'"${note_default}"
+else
+    notes="**Changelog**"$'\n\n'"${note_features}"$'\n\n'"${note_default}"
+fi
+
+gh release create "${device}-${zip_date}" "${gh_args[@]}" --title "${device}-${zip_date} build" --notes "${notes}"
